1c34eb2964c7cb802ffcd772b455758611fb01e2
Jani Metež <janimetez15@gmail.com>
2019-03-09 21:18:53 +0100

    regresijska premica

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,123 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
 
-# Število skupin
+#############################################################
+#REGRESIJSKA PREMICA ZA ŠTEVILO UMRLIH V CESTNOPROMETNIH NESREČAH V NASLEDNJIH LETIH
+
+grup <- group_by(umrli, Leto)
+umrli_slo <- summarise(grup, Stevilo=(sum(Umrli, na.rm = TRUE)))
+umrli_slo <- transform(umrli_slo, Stevilo = (Stevilo / 12))
+umrli_slo$Stevilo <- round(umrli_slo$Stevilo, digits=2)
+prileg <- lm(data = umrli_slo, Stevilo ~ Leto )
+m <- data_frame(Leto=seq(2018,2022,1))
+napoved <- mutate(m, Stevilo=predict(prileg,m))
+napoved$Stevilo <- round(napoved$Stevilo, digits = 2)
+graf_regresija_umrli <- ggplot(umrli_slo, aes(x=Leto, y=Stevilo)) +
+  geom_smooth(method=lm, fullrange = TRUE, color = 'blue') +
+  geom_point(data=napoved, aes(x=Leto, y=Stevilo), color='red', size=2) +
+  geom_point() +
+  labs(title='Napoved števila smrti v prometu v naslednjih letih', y="Stevilo")
+
+print(graf_regresija_umrli)
+
+# REGRESIJSKA PREMICA ZA ŠTEVILO OSEBNIH AVTOMOBILOV NA 1000 PREBIVALCEV V PRIHODNOSTI
+avti <- group_by(avtomobili,Leto)
+avti1 <- summarise(avti, Stevilo=sum(Avtomobili, na.rm=TRUE))
+avti_regije <- transform(avti1, Stevilo = Stevilo / 12)
+avti_regije$Stevilo <- round(avti_regije$Stevilo, digits=2)
+prileg <- lm(data = avti_regije, Stevilo ~ Leto )
+napoved_avti <- mutate(m, Stevilo=predict(prileg, m))
+
+graf_regresija_avti <- ggplot(avti_regije, aes(x=Leto, y=Stevilo)) +
+  geom_smooth(method=lm, fullrange = TRUE, color = 'blue') +
+  geom_point(data=napoved_avti, aes(x=Leto, y=Stevilo), color='red', size=2) +
+  geom_point() +
+  labs(title='Napoved števila smrti v prometu v naslednjih letih', y="Stevilo")
+
+print(graf_regresija_avti)
+
+##################################################################################
+# CLUSTER REGIJ ŠTEVILO UMRLIH V CESTNOPROMETNIH NESREČAH
+
+umrli
+grp <- group_by(umrli, Regija)
+umrli_sum <- summarise(grp, vsote=sum(Umrli, na.rm = TRUE))
+
+umrli_e <- dcast(umrli, Regija~Leto, value.var = 'Umrli' )
+umrli_e <- left_join(umrli_e, umrli_sum, by = 'Regija')
+umrli_e <- umrli_e[order(umrli_e$vsote, decreasing = FALSE),]
+umrli_b <- umrli_e[,-19]
+umrli_b <- umrli_b[,-1]
+
 n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+fit <- hclust(dist(scale(umrli_b)))
+skupine4 <- cutree(fit, n)
+
+cluster4 <- mutate(umrli_e, skupine4)
+cluster4 <- cluster4[,-2:-19]
+colnames(cluster4) <- c("Regija", "Umrli")
+# zemljevid
+
+regije <- unique(zemljevid$NAME_1)
+regije <- as.data.frame(regije, stringsAsFactors=FALSE) 
+names(regije) <- "Regija"
+skupaj <- left_join(regije, cluster4, by="Regija")
+
+
+zemljevid_cluster_umrli <- ggplot() + geom_polygon(data=left_join(zemljevid, skupaj, by=c("NAME_1"="Regija")),
+                                              aes(x=long, y=lat, group=group, 
+                                                  fill=factor(Umrli))) +
+  geom_line() +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  guides(fill=guide_colorbar(title="Skupine")) +
+  ggtitle('Razvrstitev regij v skupine glede na število umrlih v cestnoprometnih nesrečah') +
+  labs(x = " ") +
+  labs(y = " ") +
+  scale_fill_brewer(palette="YlOrRd", na.value = "#e0e0d1") 
+
+print(zemljevid_cluster_umrli)
+
+####################################################################################
+# CLUSTER REGIJ GLEDE NA BRUTO PLAČO
+
+placa
+grp <- group_by(placa, Regija)
+placa_sum <- summarise(grp, vsote=sum(Placa, na.rm = TRUE))
+
+placa_a <- dcast(placa, Regija~Leto, value.var = 'Placa' )
+placa_a <- left_join(placa_a, placa_sum, by = 'Regija')
+placa_a <- placa_a[order(placa_a$vsote, decreasing = FALSE),]
+placa_b <- placa_a[,-15]
+placa_b <- placa_b[,-1]
+
+n <- 6
+fit <- hclust(dist(scale(placa_b)))
+skupine <- cutree(fit, n)
+
+cluster4 <- mutate(placa_a, skupine)
+cluster4 <- cluster4[,-2:-15]
+colnames(cluster4) <- c("Regija", "Placa")
+# zemljevid
+
+skupaj <- left_join(regije, cluster4, by="Regija")
+
+
+zemljevid_cluster_place <- ggplot() + geom_polygon(data=left_join(zemljevid, skupaj, by=c("NAME_1"="Regija")),
+                                                   aes(x=long, y=lat, group=group, 
+                                                       fill=factor(Placa))) +
+  geom_line() +
+  theme(axis.text.x=element_blank(), axis.ticks.x=element_blank(), axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  guides(fill=guide_colorbar(title="Skupine")) +
+  ggtitle('Razvrstitev regij v skupine glede na povprečno bruto plačo') +
+  labs(x = " ") +
+  labs(y = " ") +
+  scale_fill_brewer(palette="YlOrRd", na.value = "#e0e0d1") 
+
+print(zemljevid_cluster_place)
+
+
+#==================================================================================
+
+
