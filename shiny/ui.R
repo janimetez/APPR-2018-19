d33e8e88e5ff457b6fabd378b8f21e6243d63c98
Jani Metež <janimetez15@gmail.com>
2019-03-11 22:18:17 +0100

    shiny za moje funkcije

--- shiny/ui.R
+++ shiny/ui.R
@@ -2,16 +2,23 @@ library(shiny)
 
 shinyUI(fluidPage(
   
-  titlePanel("Slovenske občine"),
-  
   tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
-    )
+    tabPanel("Število umrlih v cestnoprometnih nesrečah na 10.000 prebivalcev po regijah",
+             sidebarPanel(
+               checkboxGroupInput(inputId="regija", label = "Izberi regije:",
+                                  choiceNames=c("Gorenjska", "Goriška", "Jugovzhodna", "Koroška", "Obalno-kraška", "Osrednjeslovenska", "Podravska", "Pomurska", "Posavska", "Primorsko-notranjska", "Savinjska", "Zasavska"),
+                                  choiceValues =c("Gorenjska", "Goriška", "Jugovzhodna", "Koroška", "Obalno-kraška", "Osrednjeslovenska", "Podravska", "Pomurska", "Posavska", "Primorsko-notranjska", "Savinjska", "Zasavska"),
+                                  selected = "Jugovzhodna")
+             ),
+             mainPanel(plotOutput("regija1")))
+
+             ),
+   tabPanel("Leto",
+           sidebarPanel(
+             sliderInput("Leto", "Izberi leto", min = 2001, max = 2017,
+                         value = 2001, step = 1, sep='', animate = animationOptions(interval=250))
+           ),
+           mainPanel(plotOutput("avto"))
+  )
 ))
+
