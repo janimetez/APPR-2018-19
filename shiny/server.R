c69065fe0f99290b22b2c13a7497f2a24c2c81b5
Jani Metež <janimetez15@gmail.com>
2019-03-11 22:18:04 +0100

    shiny server

--- shiny/server.R
+++ shiny/server.R
@@ -1,24 +1,11 @@
 library(shiny)
-
-shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    dcast(druzine, obcina ~ velikost.druzine, value.var="stevilo.druzin") %>%
-      rename(`Občina`=obcina)
+      
+function(input, output) {
+  output$regija1 <- renderPlot({
+    graf.regije(input$regija)
+  })
+  output$avto <- renderPlot({
+    graf_avto(input$Leto) + theme(axis.text.x = element_text(angle = 90, hjust = 1))
   })
   
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
-    } else {
-      t <- obcine
-    }
-    ggplot(t, aes(x=naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
-  })
-})
+}
