b71c7a5d78dc6351acb31362563b545ab8417f8a
Jani Metež <janimetez15@gmail.com>
2019-03-16 09:18:02 +0100

    dodaten paket iz knjižnice

--- lib/libraries.r
+++ lib/libraries.r
@@ -4,6 +4,7 @@ library(gsubfn)
 library(reshape2)
 library(shiny)
 library(readr)
+library(rgdal)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
